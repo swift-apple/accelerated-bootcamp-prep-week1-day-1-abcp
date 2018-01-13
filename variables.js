1a34d25e0b8b336f3980d5383db701020c3f3f67
Melissa Howard <mhoward8701@gmail.com>
2018-01-13 17:23:04 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -8,8 +8,8 @@ console.log(personality)
 personality = "fabulous";
 console.log(personality);
 
-// Your Turn!
-// change the value of the `personality` variable to something that describes you
-// and console.log the new value
+personality = "awesome";
+console.log(personality)
+
 
 
