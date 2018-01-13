9643fe6580cb0ffc1ab0d5212bff5f7f6a854f2f
Melissa Howard <mhoward8701@gmail.com>
2018-01-13 17:29:47 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -4,8 +4,12 @@ console.log("You have earned " + beyoncesMoney)
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
-var totalSum = 500 + 750 + 3000 + 5100 +12980 + 9087 + 50000
-
+var beyoncesMoney = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
+console.log("Dear Beyonce,")
+console.log("We have calculated your total earnings this year.")
+console.log("You have earned " + beyoncesMoney)
+console.log("Of" + beyoncesMoney + " some portion derives from your live performances.")
+console.log("Of" + beyoncesMoney + " some portion derives from music sales.");
 
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
